c581c2ab0addec59f7fe62ef623893430ccfdae0
J.Knight <jason25840@gmail.com>
2024-08-26 11:30:53 -0400

    add author button

--- src/Create.js
+++ src/Create.js
@@ -5,8 +5,14 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [authors, setAuthors] = useState(["mario", "yoshi", "luigi"]);
   const [isLoading, setIsLoading] = useState(false);
   const history = useHistory();
+
+  const handleAddAuthor = () => {
+    const newAuthor = `Author ${authors.length + 1}`;
+    setAuthors([...authors, newAuthor]);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -21,7 +27,6 @@ const Create = () => {
     }).then(() => {
       console.log("new blog added");
       setIsLoading(false);
-      //history.go(-1);
       history.push("/");
     });
   };
@@ -45,14 +50,33 @@ const Create = () => {
         ></textarea>
         <label>Blog Author:</label>
         <select value={author} onChange={(e) => setAuthor(e.target.value)}>
-          <option value="mario">Mario</option>
-          <option value="yoshi">Yoshi</option>
+          {authors.map((author, index) => (
+            <option key={index} value={author}>
+              {author}
+            </option>
+          ))}
         </select>
-        {!isLoading && <button>Add Blog</button>}
-        {isLoading && <button disabled>Adding Blog...</button>}
-        <p>{title}</p>
+        <div className="buttons">
+          {!isLoading && (
+            <button
+              className="button_add-author"
+              type="button"
+              onClick={handleAddAuthor}
+            >
+              Add Author
+            </button>
+          )}
+          {isLoading && <button disabled>Adding Author...</button>}
+          {!isLoading && (
+            <button className="button_add-blog" type="submit">
+              Add Blog
+            </button>
+          )}
+          {isLoading && <button disabled>Adding Blog...</button>}
+        </div>
+        {/*<p>{title}</p>
         <p>{body}</p>
-        <p>{author}</p>
+        <p>{author}</p>*/}
       </form>
     </div>
   );
