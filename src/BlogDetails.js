d937b6cf3396465f70d9ea853f72fd01723edfa8
J.Knight <jason25840@gmail.com>
2024-08-25 20:22:21 -0400

    basic blog framework

---
+++ src/BlogDetails.js
@@ -0,0 +1,40 @@
+import {
+  useHistory,
+  useParams,
+} from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./useFetch";
+
+const BlogDetails = () => {
+  const { id } = useParams();
+  const {
+    data: blog,
+    isLoading,
+    error,
+  } = useFetch("http://localhost:8000/blogs/" + id);
+  const history = useHistory();
+
+  const handleDeleteClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
+
+  return (
+    <div className="blog-details">
+      {isLoading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+          <button onClick={handleDeleteClick}>Delete</button>
+        </article>
+      )}
+    </div>
+  );
+};
+
+export default BlogDetails;
---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import "./blocks/navbar.css";
+
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Basis Create Blog</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/Create">New Blog</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
